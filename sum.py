66bb30f37d4b16a6848a61a01fb225e719973054
Aarni Koskela <akx@iki.fi>
2018-07-18 17:04:18 +0300

    Add a little more verbosity

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
-# From https://cloud.google.com/tpu/docs/quickstart
+# Based on https://cloud.google.com/tpu/docs/quickstart
 import os
 import tensorflow as tf
 from tensorflow.contrib import tpu
@@ -7,30 +7,46 @@ from tensorflow.contrib.cluster_resolver import TPUClusterResolver
 tpu_name = os.environ.get('TPU_NAME')
 use_tpu = bool(tpu_name)
 
+
 def axy_computation(a, x, y):
-  return a * x + y
+    return a * x + y
+
+output_shape = [80, 80]
 
 inputs = [
     3.0,
-    tf.ones([80, 80], tf.float32),
-    tf.ones([80, 80], tf.float32),
+    tf.ones(output_shape, tf.float32),
+    tf.ones(output_shape, tf.float32),
 ]
 
 if use_tpu:
-  computation = tpu.rewrite(axy_computation, inputs)
-  tpu_grpc_url = TPUClusterResolver(tpu=[tpu_name]).get_master()
+    print('Setting up TPU')
+    tpu_grpc_url = TPUClusterResolver(tpu=[tpu_name]).get_master()
+    computation = tpu.rewrite(axy_computation, inputs)
 else:
-  computation = tf.py_func(axy_computation, inputs, tf.float32)
-  tpu_grpc_url = None
+    print('TPU IS NOT ENABLED (pass a TPU name or grpc://ip:port as the TPU_NAME envvar)')
+    computation = tf.py_func(axy_computation, inputs, tf.float32)
+    tpu_grpc_url = None
+
 
 with tf.Session(tpu_grpc_url) as sess:
-  if use_tpu:
-    sess.run(tpu.initialize_system())
-  sess.run(tf.global_variables_initializer())
-  output = sess.run(computation)
-  print(output)
-  
-  if use_tpu:
-    sess.run(tpu.shutdown_system())
+    if use_tpu:
+        print('Running TPU initializer')
+        sess.run(tpu.initialize_system())
+    sess.run(tf.global_variables_initializer())
+    print('Running computation {}'.format(computation))
+    output = sess.run(computation)
+    print(output)
+    output_var = tf.get_variable('output', output_shape)
+    sess.run(tf.assign(output_var, output))
+
+    if use_tpu:
+        print('Shutting down TPU')
+        sess.run(tpu.shutdown_system())
+
+    print('Saving session information')
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, os.environ.get('VH_OUTPUTS_DIR', '.') + '/model.ckpt')
+    print('OK: {}'.format(save_path))
 
 print('Done!')
